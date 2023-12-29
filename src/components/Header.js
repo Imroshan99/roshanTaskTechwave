a89889e80e7d9c1759b3cc2e45a4c41f2cfe5845
ROSHAN TAMBE <roshan.tambe@fablefintech.com>
2023-12-29 19:37:45 +0530

    Made header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <>
+      <div className="bg-warning p-2 mt-2 d-flex justify-content-center align-items-center">
+        <h1 className="text-black">Techwave IT Solution Pvt. Ltd.</h1>
+      </div>
+    </>
+  );
+}
