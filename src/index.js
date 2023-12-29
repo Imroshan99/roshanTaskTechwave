9593471c33bf4aabcca5d1693e1546be0bb7d3f8
ROSHAN TAMBE <roshan.tambe@fablefintech.com>
2023-12-29 20:17:51 +0530

    ant import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "../node_modules/antd/dist/antd.min.css";
+import "antd/dist/antd";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
