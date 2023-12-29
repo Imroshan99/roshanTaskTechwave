079a147d6ff3f1e966d511e06dddcfa1934e302b
ROSHAN TAMBE <roshan.tambe@fablefintech.com>
2023-12-29 19:38:48 +0530

    call Header and Body component in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Body from "./components/Body";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">Roshan</div>;
+  return (
+    <div className="App p-2">
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
