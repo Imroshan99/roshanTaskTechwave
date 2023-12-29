c511f50f6d4da00194ec801d6cb10cdf3227b214
ROSHAN TAMBE <roshan.tambe@fablefintech.com>
2023-12-29 23:30:28 +0530

    Header fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
+// import logo from "../assets/images/TechWave.png";
+
 import React from "react";
+
 function Header() {
   return (
     <>
-      <div className="bg-warning p-2 mt-2 d-flex justify-content-center align-items-center">
-        <h1 className="text-black">Techwave IT Solution Pvt. Ltd.</h1>
+      <div
+        style={{ background: "#424d58" }}
+        className="rounded p-2 mt-2 d-flex justify-content-center align-items-center"
+      >
+        {/* <img src={logo} width="200px" height="50" /> */}
+        <h1 style={{ fontWeight: "700" }} className="text-white fw-700">
+          Techwave IT Solution Pvt Ltd
+        </h1>
       </div>
     </>
   );
